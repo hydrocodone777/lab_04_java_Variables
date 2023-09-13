ea8808596af25c8d5b2b358070dd7517e2c63815
Nikolai Gertsman <mumunick1@gmail.com>
2023-09-13 15:17:40 -0400

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int intOperandA = 4;
+        int intOperandB = 7;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModule = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        int.Product = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        double doubleOperandA = 3.14;
+        double doubleOperandB = 3.50;
+        double doubleSum = 0;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + "is" + doubleSum);
+
     }
 }
