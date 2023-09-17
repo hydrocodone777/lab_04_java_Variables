f1235de168872e50f6370aef273f38bb7a745b8c
hydrocodone777 <144839823+hydrocodone777@users.noreply.github.com>
2023-09-17 15:27:43 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,34 @@ public class Main {
     public static void main(String[] args) {
         int intOperandA = 4;
         int intOperandB = 7;
-
         int intSum = 0;
         int intProduct = 0;
         int intDifference = 0;
-        int intQuotient = 0;
+        double intQuotient = 0;
         int intModule = 0;
 
         intSum = intOperandA + intOperandB;
         System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
 
-        int.Product = intOperandA * intOperandB;
+        intProduct = intOperandA * intOperandB;
         System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
 
+        intQuotient = (double) intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA +  "and " + intOperandB + "is " + intQuotient);
         double doubleOperandA = 3.14;
         double doubleOperandB = 3.50;
         double doubleSum = 0;
-
+        double doubleDifference = 0;
+        double doubleProduct = 0;
+        double doubleQuotient = 0;
         doubleSum = doubleOperandA + doubleOperandB;
-        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + "is" + doubleSum);
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + "is " + doubleSum);
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + "is " + doubleDifference);
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + "is " + doubleProduct);
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + "is " + doubleQuotient);
 
     }
 }
